7f92f63cdf299aaac4a51c493396950422a32dee
Jeevan1975 <jeevang1975@gmail.com>
2025-05-31 12:46:32 +0530

    Integrate payment summary rendering into order summary; update cart item removal to trigger payment summary refresh

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -2,12 +2,8 @@ import { cart, removeFromCart, updateDeliveryOption } from "../../data/cart.js";
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js"
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js"
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
-
 
 export function renderOrderSummary() {
 
@@ -119,6 +115,8 @@ export function renderOrderSummary() {
 
       const container = document.querySelector(`.js-cart-item-container-${productId}`)
       container.remove();
+
+      renderPaymentSummary();
     });
   });
 
